8b50c140b60e54bb778cbb37ba17e02a36a8cbed
cmander <cmcov@live.com>
2015-10-07 21:08:10 +0100

    Update and rename Warmup: 06 - pos_neg to Warmup-1: 06 - pos_neg.py

--- Warmup-1: 06 - pos_neg.py
+++ Warmup-1: 06 - pos_neg.py
@@ -8,7 +8,3 @@ def pos_neg(a, b, negative):
         if (a >= 0 and b <= 0) or (a <= 0 and b >= 0):
             return True
     return False
-
-
-print(pos_neg(-2, 1, False))
-print(pos_neg(1, 1, True))
